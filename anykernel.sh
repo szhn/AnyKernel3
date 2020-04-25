3010d0978396d2bdbb27f12458cc2479df94a34d
Alex <53017714+spacetree1@users.noreply.github.com>
2020-04-25 13:29:40 +0300

    sirius: add specific stuff

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=IceCream by TheMalachite
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
+device.name1=sirius
 device.name2=
 device.name3=
 device.name4=
@@ -20,7 +20,7 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=
+block=/dev/block/bootdevice/by-name/boot
 is_slot_device=0;
 ramdisk_compression=auto;
 
